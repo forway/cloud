180e509d103c5f9b390b559180b1503bcb2b86d3
forway <1609424788@qq.com>
2017-03-03 21:52:33 +0800

    test

--- src/main/java/bigdata/cloud/es/pool/ESClientPool.java
+++ src/main/java/bigdata/cloud/es/pool/ESClientPool.java
@@ -14,18 +14,19 @@ import bigdata.cloud.system.CloudSystemConfig;
  * 
  * @author hongliang
  * ES client 对象池  
+ * 使用说明：获取es client对象进行查询等操作后，需要释放client对象
+ * 获取es client对象：getClient()
+ * 释放es client对象：release(Client c)
  *
  */
 public class ESClientPool{
 	
-	public static GenericObjectPool<Client> pool;
+	private static GenericObjectPool<Client> pool;
 	//使用单例模式
 
 	private ESClientPool() {
 		// 初始化
-		if(pool == null || pool.getNumActive() == 0){
-			initPool();
-		}
+		initPool();
 	}
 	
 	/**
@@ -42,26 +43,6 @@ public class ESClientPool{
 	 */
 	public static ESClientPool getInstance(){
 		return SingletonHolder.esClientPool;
-	}
-	
-	/**
-	 * 初始化资源池
-	 */
-	public static void initPool(){
-		
-		Settings esSettings = Settings.settingsBuilder().put("cluster.name", CloudSystemConfig.es_clusterName)
-				.put("client.transport.sniff", true).build();
-		//设置多个host，如果某个host出现连接问题，其它host还可以使用
-		InetSocketTransportAddress[] addressArray = getAllAddress(CloudSystemConfig.es_hosts.split(","), CloudSystemConfig.es_port);
-		
-		ESClientPoolableObjectFactory poolFactory = new ESClientPoolableObjectFactory(esSettings, addressArray);
-		GenericObjectPoolConfig config = new GenericObjectPoolConfig();
-		config.setMaxTotal(10);	//最大连接数
-		config.setMaxIdle(10);	//链接池中最大空闲的连接数，默认为8.该参数一般尽量与_maxActive相同，以提高并发数
-		config.setMinIdle(5);	//连接池中最少空闲的连接数，默认为0
-		config.setMaxWaitMillis(120 * 1000);	//当连接池资源耗尽时，调用者最大阻塞的时间，超时将跑出异常。单位，毫秒数；默认为-1.表示永不超时
-		config.setSoftMinEvictableIdleTimeMillis(30 * 60 * 1000);	//连接空闲的最小时间，达到此值后空闲链接将会被移除，且保留“minIdle”个空闲连接数。默认为-1.
-		pool = new GenericObjectPool<Client>(poolFactory, config);
 	}
 	
 	/**
@@ -88,6 +69,28 @@ public class ESClientPool{
 	}
 	
 	/**
+	 * 初始化资源池
+	 */
+	private static void initPool(){
+		
+		if(pool == null || pool.isClosed()){
+			Settings esSettings = Settings.settingsBuilder().put("cluster.name", CloudSystemConfig.es_clusterName)
+					.put("client.transport.sniff", true).build();
+			//设置多个host，如果某个host出现连接问题，其它host还可以使用
+			InetSocketTransportAddress[] addressArray = getAllAddress(CloudSystemConfig.es_hosts.split(","), CloudSystemConfig.es_port);
+			
+			ESClientPoolableObjectFactory poolFactory = new ESClientPoolableObjectFactory(esSettings, addressArray);
+			GenericObjectPoolConfig config = new GenericObjectPoolConfig();
+			config.setMaxTotal(CloudSystemConfig.es_clientPoolSize); //最大连接数
+			config.setMaxIdle(CloudSystemConfig.es_clientPoolSize);	//链接池中最大空闲的连接数，默认为8.该参数一般尽量与_maxActive相同，以提高并发数
+			config.setMinIdle(5);	//连接池中最少空闲的连接数，默认为0
+			config.setMaxWaitMillis(120 * 1000);	//当连接池资源耗尽时，调用者最大阻塞的时间，超时将跑出异常。单位，毫秒数；默认为-1.表示永不超时
+			config.setSoftMinEvictableIdleTimeMillis(30 * 60 * 1000);	//连接空闲的最小时间，达到此值后空闲链接将会被移除，且保留“minIdle”个空闲连接数。默认为-1.
+			pool = new GenericObjectPool<Client>(poolFactory, config);
+		}
+	}
+	
+	/**
      * 获得所有的地址端口的netSocketTransportAddress数组
      * @return
      */
@@ -102,6 +105,5 @@ public class ESClientPool{
 		}
         return addressList;
     }
-	
 	
 }
--- src/main/java/bigdata/cloud/system/CloudSystemInit.java
+++ src/main/java/bigdata/cloud/system/CloudSystemInit.java
@@ -1,18 +1,12 @@
 package bigdata.cloud.system;
 
-import java.io.IOException;
-import java.util.Map;
-
 import javax.annotation.PostConstruct;
 
 import org.apache.log4j.Logger;
-import org.elasticsearch.client.Client;
-import org.elasticsearch.cluster.ClusterState;
-import org.elasticsearch.cluster.metadata.IndexMetaData;
-import org.elasticsearch.cluster.metadata.MappingMetaData;
 import org.springframework.stereotype.Component;
 
 import bigdata.cloud.es.pool.ESClientPool;
+import bigdata.cloud.redis.pool.RedisClientPool;
 
 /**
  * cloud系统初始化类
@@ -33,48 +27,11 @@ public class CloudSystemInit {
 		//加载配置文件信息
 		CloudSystemConfig.initConfigInfo();
 		//初始化ES客户端对象池
-		ESClientPool.initPool();
+		ESClientPool.getInstance();
 		//初始化redis客户端对象池
-		//RedisClientPool.initPool();
+		RedisClientPool.initPool();
 		
 		logger.info("cloud system init finished.");
-		
-		//test
-		for(int i=0; i<2500; i++){
-			
-			new Thread(new Runnable() {
-				
-				@Override
-				public void run() {
-					Client client = ESClientPool.getInstance().getClient();
-
-			    	String index = "elasticsearch-sql_test_index";  //elasticsearch-sql_test_index    linux-syslog-2016.12.20
-			    	String type = "account";   //account   logs
-			    	ClusterState cs = client.admin().cluster().prepareState().setIndices(index).execute().actionGet()
-							.getState();
-					IndexMetaData imd = cs.getMetaData().index(index);
-					MappingMetaData mdd = imd.mapping(type);
-					Map<String, Object> map = null;
-					try
-					{
-						map = mdd.getSourceAsMap();
-						for(String s : map.keySet()){
-							System.out.println(s + " --- " + map.get(s));
-						}
-					} catch (IOException e)
-					{
-						e.printStackTrace();
-					}
-
-					ESClientPool.getInstance().release(client);
-					
-				}
-			}).start();
-			
-		}
-		
-		
-		
 		
 		
 	}
